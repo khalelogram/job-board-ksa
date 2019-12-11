3e2c1aafdc20b87773093b3fa5b7cd47a31bef06
GeloGuevarra <abrahampoog@gmail.com>
2019-12-11 14:00:38 +0800

    added library and template

---
+++ bam/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Job Board</title>
+	<!-- RESPONSIVE TAG -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="./lib/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<!-- Start HTML Here -->
+	<div id="main">
+
+
+
+	</div>
+	<!-- END HTML Here -->
+	<!-- Jquery Plugin -->
+	<script type="text/javascript" src="./lib/jquery/jquery.min.js"></script>
+	<!-- Bootstrap Plugin -->
+	<script type="text/javascript" src="./lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
